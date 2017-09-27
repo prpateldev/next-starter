926a15b519f4579bdee9b81a5bf249b5e2e18ef7
Piyush Patel <piyush.patel@actonate.com>
2017-09-27 18:02:45 +0530

    code runs on server and clienside

--- pages/dynamic.js
+++ pages/dynamic.js
@@ -2,8 +2,14 @@ import Menu from '../app/components/Menu';
 import PageHead from '../app/components/PageHead';
 import PageScripts from '../app/components/PageScripts';
 import DynamicComponents from '../app/components/DynamicComponents';
-const Dynamic = (props) => {
-    return <div>
+class Dynamic extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(`From constructor() "${props.msgFromServer}"`)
+    }
+    
+    render() {
+        return <div>
             <PageHead />
             <Menu activeLink="/dynamic"/>
             <div className="container">
@@ -13,6 +19,16 @@ const Dynamic = (props) => {
             </div>
             <PageScripts />
         </div>
+    }
+}
+
+Dynamic.getInitialProps = async () => {
+    console.log('Runs every time .getInitialProps() execute');
+    if(!process.browser) {
+        // This code will render only on server
+        console.log('Dynamic.getInitialProps() execute on server');
+    }
+    return {msgFromServer: 'Hello PR'};
 }
 
 export default Dynamic;
